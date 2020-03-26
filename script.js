b23eb3aea12d305e0905afc1c17bd75df5057f51
JidMenosta <jidmenosta@gmail.com>
2020-03-26 15:22:01 -0400

    added all my divs for content, js functions, and css style

--- script.js
+++ script.js
@@ -9,6 +9,14 @@ var uvIndex = 'https://api.openweathermap.org/data/2.5/uvi?appid=f3a53b113ee3edc
 var myWeatherData = ['', '', '', ];
 
 $.get(myCurrentCityURL, function(response) {
+
+    var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+
+    $("#city").html("<h1> City: " + response.name + "</h1>");
+    $("#condition").html("<h2> Condition: " + response.weather[0].description + "</h2>");
+    $("#temp").html("<h2> Temp: " + tempF.toFixed(2) + " F</h2>");
+    $("#humidity").html("<h2> Humidity: " + response.main.humidity + "</h2>");
+    $("#windSpeed").html("<h2> Wind Speed: " + response.wind.speed + "</h2>");
     console.log(response);
 })
 
@@ -23,10 +31,23 @@ $.get(myCityURL5, function(response) {
         var myURL = "<img src='http://openweathermap.org/img/wn/" + myImage + "@2x.png' />";
         console.log("myURL", myURL)
 
-        $(".imgDiv").html(myURL);
+        $("#weatherIcon").html(myURL);
     }
 })
 
 $.get(uvIndex, function(response) {
+    $("#uvIndex").html("<h2> UV Index: " + response.value + "</h2>");
     console.log("My UV Index is ", response.value);
+
+    if (response.value <= 2) {
+        $("#uvIndex").css({ "background-color": "green", "color": "snow" });
+    } else if (response.value <= 5) {
+        $("#uvIndex").css({ "background-color": "yellow", "color": "black" });
+    } else if (response.value <= 7) {
+        $("#uvIndex").css({ "background-color": "orange", "color": "snow" });
+    } else if (response.value <= 10) {
+        $("#uvIndex").css({ "background-color": "red", "color": "snow" });
+    } else {
+        $("#uvIndex").css({ "background-color": "purple", "color": "snow" });
+    };
 })
